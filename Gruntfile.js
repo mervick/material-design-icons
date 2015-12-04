f83c839aa32b046d9df91f01ae0b1966b2e8fba2
Andrey Izman <izman.dev@gmail.com>
2015-12-04 05:58:00 +0200

    Add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    sourcemap: 'none',
+                    unixNewlines: true,
+                    compass: true,
+                    lineNumbers: false,
+                },
+                files: {
+                    'css/material-icons.css' : 'scss/material-icons.scss'
+                }
+            },
+        },
+        cssmin: {
+            options: {
+                sourceMap: true
+            },
+            target: {
+                files: {
+                    'css/material-icons.min.css': ['css/material-icons.css']
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'scss/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['cssmin', 'watch']);
+}
