bab12df60fb54841a3d1641f738e6e1949987be0
Andrey Izman <izman.dev@gmail.com>
2015-12-04 14:43:42 +0200

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,5 +34,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['cssmin', 'watch']);
+    grunt.registerTask('default', ['sass', 'cssmin', 'watch']);
 }
