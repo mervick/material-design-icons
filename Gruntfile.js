e8908cfdcd8a88efa600eb30b8a546f3382c4d9d
Andrey Izman <izmanw@gmail.com>
2021-01-18 23:31:32 +0200

    Update repo, generate codepoints and scss, compile css, download fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,91 @@
-/* jshint node: true */
-"use strict";
-
-var livereload = {
-    host: 'localhost',
-    port: 35729,
-};
+const fonts = [
+  'regular', 'outlined', 'round', 'sharp', 'two-tone'
+];
 
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        sass: {
-            dist: {
-                options: {
-                    sourcemap: 'none',
-                    unixNewlines: true,
-                    compass: true,
-                    lineNumbers: false,
-                },
-                files: {
-                    'css/material-icons.css' : 'scss/material-icons.scss'
-                }
-            },
-            demo: {
-                options: {
-                    sourcemap: 'none',
-                    unixNewlines: true,
-                    compass: true,
-                    lineNumbers: true,
-                },
-                files: {
-                    'demo/style/main.css' : 'demo/style/main.scss'
-                }
-            },
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        options: {
+          sourcemap: 'none',
+          unixNewlines: true,
+          lineNumbers: false,
         },
-        cssmin: {
-            options: {
-                sourceMap: true
-            },
-            target: {
-                files: {
-                    'css/material-icons.min.css': ['css/material-icons.css']
-                }
-            }
+        files: [
+          // // fonts + icons bundle
+          // ...fonts.map(font => ({
+          //   dest: `css/material-icons-${font}-bundle.css`,
+          //   src: [
+          //     'scss/material-icons.scss',
+          //     `scss/material-icons-${font}.scss`
+          //   ]
+          // })),
+          {
+            // only fonts
+            ...Object.fromEntries(fonts.map(font => ([
+              `css/material-icons-${font}.css`, `scss/material-icons-${font}.scss`
+            ]))),
+            // // fonts + icons bundle
+            // ...Object.fromEntries(fonts.map(font => ([
+            //   `css/material-icons-${font}-bundle.css`, [
+            //     `scss/material-icons-${font}.scss, scss/material-icons.scss`
+            //   ]
+            // ]))),
+            // only icons
+            'css/material-icons-base.css': 'scss/base.scss',
+            // all fonts + icons bundle
+            'css/material-icons.css': 'scss/bundle.scss'
+          }
+        ],
+      },
+      demo: {
+        options: {
+          sourcemap: 'none',
+          unixNewlines: true,
+          lineNumbers: true,
         },
-        watch: {
-            css: {
-                files: [
-                    'scss/*.scss',
-                    'demo/style/*.scss'
-                ],
-                tasks: ['sass'],
-                options: {
-                    livereload: livereload
-                }
-            }
+        files: {
+          'demo/style/main.css': 'demo/style/main.scss'
         }
-    });
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['sass', 'watch']);
+      },
+    },
+    cssmin: {
+      options: {
+        sourceMap: true
+      },
+      target: {
+        files:  [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    watch: {
+      css: {
+        files: [
+          'scss/*.scss',
+          'demo/style/*.scss'
+        ],
+        tasks: ['sass']
+      }
+    },
+    shell: {
+      updateRepo: {
+        options: {
+          stdout: true
+        },
+        command: 'python3 scripts/update_repo.py'
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('build', ['shell', 'sass', 'cssmin']);
+  grunt.registerTask('default', ['sass', 'watch']);
 }
