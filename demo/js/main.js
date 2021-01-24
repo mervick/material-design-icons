7ce8f06ea45696a6960fef604e2650cb23eb9b24
Andrey Izman <izmanw@gmail.com>
2021-01-24 11:02:48 +0200

    Update demo

--- demo/js/main.js
+++ demo/js/main.js
@@ -7,57 +7,116 @@
 
   const Models = {};
   const Views = {};
+  const Icons = {};
 
-  const families = {
-    "Material Icons": 'regular',
-    "Material Icons Outlined": 'outlined',
-    "Material Icons Round": 'round',
-    "Material Icons Sharp": 'sharp',
-    "Material Icons Two Tone": 'two-tones'
+  const themes = {
+    'REGULAR': "Material Icons",
+    'OUTLINED': "Material Icons Outlined",
+    'ROUND': "Material Icons Round",
+    'SHARP': "Material Icons Sharp",
+    'TWO_TONE': "Material Icons Two Tone",
   };
 
-  let activeFont = "Material Icons";
+  const themesArr = Object.keys(themes);
 
-  const renderData = function (dataIcons) {
+  const fonts =_.invert(themes)
+
+  const mdiClasses = {
+    "REGULAR": 'mdi',
+    "OUTLINED": 'mdi-outlined',
+    "ROUND": 'mdi-round',
+    "SHARP": 'mdi-sharp',
+    "TWO_TONE": 'mdi-two-tone'
+  };
+
+  const ligatureClasses = {
+    "REGULAR": 'material-icons',
+    "OUTLINED": 'material-icons-outlined',
+    "ROUND": 'material-icons-round',
+    "SHARP": 'material-icons-sharp',
+    "TWO_TONE": 'material-icons-two-tone'
+  };
+
+  const THEME_ALL = 'THEME_ALL';
+  let activeTheme = THEME_ALL;
+
+  const fix_unsupported = {
+    content_copy: ["Material Icons"],
+    content_cut: ["Material Icons"],
+    content_paste: ["Material Icons"]
+  };
+
+  let latest = 0;
+  function debug(name, start) {
+    const now = performance.now();
+    console.log(name, Math.floor((now - start) * 100) / 100, Math.floor((now - latest) * 100) / 100);
+    latest = now;
+  }
+
+  const appViews = {};
+  const getView = function(name, args) {
+    return appViews[name] || (appViews[name] = new Views[name](args));
+  }
+
+  const renderApp = function () {
+    Icons.hash = {};
     let icons = [];
     const categories = {};
-    dataIcons.forEach(function(icon) {
+
+    window.metadata.icons.forEach(function(icon) {
       (icon.categories || []).forEach(function(category) {
         const name = icon.name || '';
-        if (icon.unsupported_families.indexOf(activeFont) !== -1) return;
-        categories[category] || (categories[category] = []);
-        categories[category] = categories[category].concat({
+        let themeIndex = 0;
+        let theme = activeTheme === THEME_ALL ? themesArr[themeIndex] : activeTheme;
+        let font = themes[theme];
+
+        while (icon.unsupported_families.indexOf(font) !== -1 ||
+          (fix_unsupported[name] && fix_unsupported[name].indexOf(font) !== -1)) {
+          if (activeTheme === THEME_ALL && themeIndex < 4) {
+            theme = themesArr[++themeIndex];
+            font = themes[theme];
+          } else {
+            return;
+          }
+        }
+
+        const caption = name.replace(/_/g, ' ');
+        const data = {
+          name: name,
+          mdiClass: mdiClasses[theme],
+          ligatureClass: ligatureClasses[theme],
+          font: font,
           category: category,
-          caption: name.replace(/_/g, ' '),
-          highlighted: name.replace(/_/g, ' '),
+          caption: caption,
+          highlighted: caption,
           className: name.replace(/_/g, '-'),
           content: name,
           tags: icon.tags || [],
           version: icon.version,
-          unsupported: icon.unsupported_families.map(family => families[family]),
-          font: activeFont
-        });
+        };
+
+        (categories[category] || (categories[category] = [])).push(data);
+        if (!Icons.hash[name]) {
+          Icons.hash[name] = data;
+        }
       })
     });
+
     const keys = Object.keys(categories);
     keys.sort();
     keys.forEach(category => {
-      icons = icons.concat(categories[category]);
+      Array.prototype.push.apply(icons, categories[category]);
     })
-    const view = new Views.Icons({collection: new Models.Icons(icons)});
-    view.render();
+    Icons.list = icons;
+
+    getView('Icons').render();
+    getView('Sidebar').render();
   };
-
-  Models.Icon = Backbone.Model.extend();
-
-  Models.Icons = Backbone.Collection.extend({
-    model: Models.Icon
-  });
 
   Views.Icon = Backbone.View.extend({
     tagName: 'div',
     className: 'item-container',
-    template: _.template($('#grid-item').html()),
+    template: _.template($('#grid-item').html(), null, {variable: "icon"}),
     events : {
       "click" : "showSnackBar"
     },
@@ -76,7 +135,7 @@
       $body.click();
       this.model.trigger('hideSnackBar');
       $(this.el).addClass("selected");
-      const view = new Views.snackbarView({model: this.model});
+      const view = new Views.Snackbar({model: this.model});
       navigate(this.model.attributes.className);
       view.render();
       return false;
@@ -88,12 +147,11 @@
     }
   });
 
-  Views.snackbarView = Backbone.View.extend({
+  Views.Snackbar = Backbone.View.extend({
     container: $('#snackbar'),
     template: _.template($('#snackbar-template').html()),
 
-    initialize: function (options) {
-      this.collection = options.collection;
+    initialize: function () {
       $body.on("click.snackbar focus.snackbar", $.proxy(this.hide, this));
       this.container.on('click', ".container", e => {
         if (e.target.tagName == 'A') {
@@ -105,10 +163,10 @@
       this.container.on('click', '.clear-icon', $.proxy(this.hide, this));
     },
 
-    render: function () {
+    render: function (model) {
       let hidden = !this.container.children(".container:not(:hidden)").length;
       this.container.empty();
-      this.container.append(this.template(this.model.attributes));
+      this.container.append(this.template(model));
       if (hidden) {
         this.container.children(".container").hide().slideDown('fast');
       } else {
@@ -117,93 +175,136 @@
     },
 
     hide: function() {
-      this.model.trigger('hideSnackBar');
+      getView('Icons').hideSnackBar();
       this.container.children(".container").slideUp('fast');
-      $body.off("click.snackbar focus.snackbar");
-      this.container.off('click');
+      // $body.off("click.snackbar focus.snackbar");
+      // this.container.off('click');
     }
+  });
+
+  Views.Sidebar = Backbone.View.extend({
+    container: $('#sidebar'),
+    template: _.template($('#sidebar-template').html()),
+
+    initialize: function (options) {
+      const self = this;
+      $doc.on('click', '#sidebar .sidebar-themes > li', function() {
+        const theme = $(this).data('theme');
+        activeTheme = theme;
+        self.container.off("click");
+        renderApp();
+      });
+      _.bindAll(this, 'render');
+    },
+
+    render: function () {
+      $(this.container).html(this.template({theme: activeTheme}));
+      return this;
+    },
   });
 
   Views.Icons = Backbone.View.extend({
     container: $('#grid-container'),
     empty_content: $('#empty-grid').html(),
+    icon_template: _.template($('#grid-item').html(), null, {variable: "icon"}),
     search_input: $('#search'),
     search_clear: $('#search-panel .clear-icon'),
 
-    initialize: function (options) {
-      this.collection = options.collection;
+    initialize: function () {
+      $doc.on('click', '#grid-container .item-container', $.proxy(this.showSnackBar, this));
       this.search_input.bind('keyup', $.proxy(this.search, this));
       this.search_clear.bind('click', $.proxy(this.clear_search, this));
-      _.bindAll(this, 'render');
+      _.bindAll(this, 'render', 'search', 'showSnackBar');
+    },
+
+    showSnackBar: function(e) {
+      this.activeIcon = $(e.currentTarget);
+      const icon = this.activeIcon.attr('data-icon');
+      const data = Icons.hash[icon];
+
+      $body.click();
+      this.activeIcon.addClass("selected");
+
+      navigate(data.name);
+      getView('Snackbar').render(data);
+      return false;
+    },
+
+    hideSnackBar: function() {
+      this.activeIcon.removeClass("selected");
+      navigate();
     },
 
     clear_search : function() {
       this.search_input.val('');
       this.search_input.focus();
-      this.search();
+      this.render();
       return this;
     },
 
-    search: function () {
-      let str = this.search_input.val();
-      if (str.length > 0) {
+    search: function() {
+      this.timer && clearTimeout(this.timer);
+      this.timer = setTimeout(this.render, 400);
+    },
+
+    render: function () {
+      let self = this;
+      const initGrid = function() {
+        return $("<div/>", {"class" : "grid"});
+      }
+      const appendCategory = function(grid, category) {
+        const cat = $("<div/>", {"class": "grid-category", "id": "cat_" + category});
+        $("<h2/>").html(category.charAt(0).toUpperCase() + category.slice(1)).appendTo(cat);
+        grid.appendTo(cat);
+        cat.appendTo(self.container);
+      }
+
+      let search = this.search_input.val();
+      const isSearch = search.length > 0;
+      if (isSearch) {
         this.search_clear.show();
       } else {
         this.search_clear.hide();
+        search = search.replace(/[\-_]+/g, ' ').replace(/\s+/, ' ').trim().toLowerCase();
       }
-      str = str.replace(/[\-_]+/g, ' ').replace(/\s+/, ' ').trim().toLowerCase();
 
-      if (str.length > 0) {
-        let models = this.collection.filter(function (item) {
-          let caption = item.get("caption");
-          if (caption.indexOf(str) > -1) {
-            item.set("highlighted", '<span>' + caption.replace(str, '</span><i>' + str + '</i><span>').replace(/\s/g, '&nbsp;') + '</span>');
+      let models;
+      if (isSearch) {
+        models = Icons.list.filter(function(item) {
+          let caption = item.caption;
+          if (caption.indexOf(search) > -1) {
+            item.highlighted = '<span>' + caption.replace(search, '</span><i>' + search + '</i><span>').replace(/\s/g, '&nbsp;') + '</span>';
             return true;
           }
           return false;
         });
-        this.render(models);
       } else {
-        let models = this.collection.each(function (item) {
-          item.set("highlighted", item.get("caption"));
-        });
-        this.render(models);
+        models = Icons.list;
       }
-      $('body, html').animate({scrollTop: this.container.offset().top - 64}, 0);
-      return this;
-    },
 
-    render: function (searchCollection) {
-      let category = null;
-      let grid = $("<div/>", {"class" : "grid"});
-      let self = this;
-      let models = searchCollection || this.collection;
+      let grid = initGrid();
       this.container.empty();
+      let category = null;
 
       models.forEach(function (item) {
-        let itemView = new Views.Icon({model: item});
-        if (category === null) {
-          category = item.get('category');
+        if (!category) {
+          category = item.category;
         }
-        if (category !== item.get('category')) {
-          $("<h2/>").html(category.charAt(0).toUpperCase() + category.slice(1)).
-            appendTo(self.container);
-          grid.appendTo(self.container);
-
-          category = item.get('category');
-          grid = $("<div/>", {"class" : "grid"});
-          grid.append(itemView.render().el);
-        } else {
-          grid.append(itemView.render().el);
+        if (category !== item.category) {
+          appendCategory(grid, category)
+          grid = initGrid();
+          category = item.category;
         }
+        grid.append(self.icon_template(item));
       });
 
-      if (category !== null) {
-        $("<h2/>").html(category.charAt(0).toUpperCase() + category.slice(1)).appendTo(this.container);
-        grid.appendTo(this.container);
+      if (category) {
+        appendCategory(grid, category)
       } else {
-        this.container.html(self.empty_content);
+        this.container.html(this.empty_content);
       }
+
+      isSearch && $('body, html').animate({scrollTop: this.container.offset().top - 64}, 0);
 
       return this;
     }
@@ -215,19 +316,20 @@
       ":query": "item",
     },
     index: function() {
-      renderData(window.metadata.icons);
+      renderApp();
     },
 
     item: function(query) {
-      renderData(window.metadata.icons);
-      setTimeout(function() {
-        let item = $(".item-container > .item > .mdi.mdi-" + query.replace(/[^0-9a-zA-Z]/g, '-'))
+      renderApp();
+      const navigateToIcon = function() {
+        let item = $(".item-container > .item > .mdi-" + query.replace(/[^0-9a-zA-Z]/g, '-'))
           .closest(".item-container");
         if (item.length) {
           item.trigger("click");
           $('body, html').animate({scrollTop: item.offset().top - 74}, 0);
         }
-      }, 50);
+      };
+      setTimeout(navigateToIcon, 1000);
     }
   });
 
